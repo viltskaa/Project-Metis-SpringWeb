07f6077be672437cb9bc961532523385bf766906
andrew <andrewstyou@gmail.com>
2024-03-03 21:35:48 +0400

    Init project

---
+++ src/main/java/com/example/projectmetis/ProjectMetisApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectmetis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMetisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMetisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectmetis/ProjectMetisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmetis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMetisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
