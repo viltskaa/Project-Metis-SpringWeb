9156dfe72137d9e560f83e369b85d83c900a66de
AnnZhimol <ankavanka731@gmail.com>
2024-03-11 16:23:40 +0400

    table entity

--- src/main/java/com/example/projectmetis/models/Table.java
+++ src/main/java/com/example/projectmetis/models/Table.java
@@ -3,23 +3,20 @@ package com.example.projectmetis.models;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Document
 @Setter
 @Getter
-public class TableTop {
+public class Table {
     @Id
     private long id;
-    private double width;
-    private double height;
-    private double perimeter;
-    private double depth;
-    private String colorMain;
-    private String colorEdge;
-    private String material;
-    @Indexed(unique = true)
     private String article;
     private long timeAssembly;
+    private List<Byte> qrCode;
+    private TableTop tableTop;
+    private long marketPlaceId;
+
 }
